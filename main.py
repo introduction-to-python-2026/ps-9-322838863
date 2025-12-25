7dd0bb8900d5ae46758fb430c40c129f874f4306
322838863 <shovalgos@gmail.com>
2025-12-25 13:35:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pandas as pd
+
 df = pd.read_csv('parkinsons.csv')
-df.head()
-df =df.dropna()
+df = df.dropna()
 
 x = df[['MDVP:Fo(Hz)', 'MDVP:Jitter(%)']]
 y = df['status']
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-x_scaled = scaler.fit_transform(X)
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
 from sklearn.neighbors import KNeighborsClassifier
 model = KNeighborsClassifier()
-model.fit(x_scaled, y)
+model.fit(X_train_scaled, y_train)
 
 from sklearn.metrics import accuracy_score
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+
+print(accuracy
